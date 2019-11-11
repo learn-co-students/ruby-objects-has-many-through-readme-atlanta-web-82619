010907e8cc06abd9f29dce7362fd41571043b9dc
Justina Kuchta <44618164+jwkuchta@users.noreply.github.com>
2019-11-11 15:06:47 -0500

    Update meal.rb

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,6 +1,9 @@
 class Meal
+    
     attr_reader :waiter , :customer , :total , :tip
+    
     @@all = []
+    
     def initialize(waiter, customer, total, tip)
         @waiter = waiter
         @customer = customer
